e180e2b80a705a0eeaf778efb09e689f1b5b0dff
BruteCoder007 <davisabragam@gmail.com>
2024-02-05 01:25:37 +0530

    second commit

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,9 +3,40 @@ import {Component} from '@angular/core';
 @Component({
     selector:`.app-server`,
     templateUrl:'./server.component.html',
-    styleUrl:'./server.component.css'
+    styleUrl:'./server.component.css',
+    styles:`
+    .online{
+        color:white;
+    }`
 })
 
 export class ServerComponent{
+    bool : boolean = false;
+    isdisable : string = 'servers cannot be created';
+    variable ;
+    status : string = 'offline'
+    timebool: boolean =false;
+    constructor(){
+        setTimeout(()=>{ this.bool=true;},2000)
+        this.status= Math.random() > 0.5 ? 'online' : 'offline' ;
+    }
+     handler(){
+        this.isdisable='server can be  created';
+    }
+    onchange=(event: Event)=>{
+        console.log(event);
+        this.variable= (<HTMLInputElement>event.target).value;
+
+    }
+    isTime(){
+
+        this.timebool = new Date().getHours()<12 ? true : false
+        console.log(new Date().getHours());
+        return this.timebool;
+    }
+    getColor(){
+        return this.status == 'online' ? 'green' : ' red';
+    }
+
     
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  selector: '[app-servers]',
   templateUrl: './servers.component.html',
   styleUrl: './servers.component.css'
 })
 export class ServersComponent {
-
+    servernames : string[] = ['server1','server2'];
+    count=3;
+    createServers(){
+      this.servernames.push('server'+this.count);
+    }
 }
